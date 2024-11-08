6508ffdea78d7296fd63f2102e945d5a461dd089
omarckg <omarfuentee@gmail.com>
2024-11-07 20:41:18 -0500

    corregido base de datos

--- login.sql
+++ login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-10-2024 a las 21:11:26
+-- Tiempo de generación: 08-11-2024 a las 02:40:16
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -46,6 +46,30 @@ INSERT INTO `asignaturas` (`id`, `nombre`, `profesor_id`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `materias`
+--
+
+CREATE TABLE `materias` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `id_programa` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `matricula`
+--
+
+CREATE TABLE `matricula` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `id_semestre` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `notas`
 --
 
@@ -66,6 +90,47 @@ INSERT INTO `notas` (`id`, `estudiante_id`, `asignatura_id`, `nota`) VALUES
 (3, 11, 2, 5.00),
 (4, 11, 3, 2.00),
 (5, 15, 1, 1.00);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `programacion`
+--
+
+CREATE TABLE `programacion` (
+  `id` int(11) NOT NULL,
+  `salon` varchar(100) DEFAULT NULL,
+  `horario` time DEFAULT NULL,
+  `id_semestre` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `programas`
+--
+
+CREATE TABLE `programas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `facultad` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `semestre`
+--
+
+CREATE TABLE `semestre` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `anio` int(11) DEFAULT NULL,
+  `fecha_inicio` date DEFAULT NULL,
+  `fecha_fin` date DEFAULT NULL,
+  `estado` enum('activo','inactivo') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -94,7 +159,8 @@ INSERT INTO `users` (`id`, `username`, `password`, `rol`, `semestre`, `email`, `
 (13, 'andres', '$2b$12$ZFIHvzSmOmtCNVBKG50sJ.6yLVFuRV7XAW/AvYIEZS6S6oNP/4822', 'admin', NULL, '', '', ''),
 (14, 'lalo', '$2b$12$nb1og6rpV13IpxDkuib/WeP0/5JMSNxwFxSl0Ai7LBoeZg.JsXxsu', 'profesor', NULL, '', '', ''),
 (15, 'cristian', '$2b$12$9UK9B8T.lqH5.ntdle6kq.9Aa35kLl1k.ODwIYLjbOSOA08z0/PAW', 'estudiante', 2, '', '', ''),
-(17, 'ruben', '$2b$12$bd/.O10sYu.rqX/Auz1sUOV/BMmqjvC8Amo5BDQp922LO5ZFaUFda', 'estudiante', 3, 'omarfuentee@gmail.com', '', '');
+(17, 'ruben', '$2b$12$bd/.O10sYu.rqX/Auz1sUOV/BMmqjvC8Amo5BDQp922LO5ZFaUFda', 'estudiante', 3, 'omarfuentee@gmail.com', '', ''),
+(18, 'jeider', '$2b$12$N5WMbSGeXWI1.iheOcXBJuJwjW/LyDpA0zXLyBb4UEpimtogMXrni', 'estudiante', 3, 'jeider@gmail.com', '213455646', 'tarjeta');
 
 --
 -- Índices para tablas volcadas
@@ -108,12 +174,47 @@ ALTER TABLE `asignaturas`
   ADD KEY `profesor_id` (`profesor_id`);
 
 --
+-- Indices de la tabla `materias`
+--
+ALTER TABLE `materias`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_programa` (`id_programa`);
+
+--
+-- Indices de la tabla `matricula`
+--
+ALTER TABLE `matricula`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_semestre` (`id_semestre`);
+
+--
 -- Indices de la tabla `notas`
 --
 ALTER TABLE `notas`
   ADD PRIMARY KEY (`id`),
   ADD KEY `estudiante_id` (`estudiante_id`),
   ADD KEY `asignatura_id` (`asignatura_id`);
+
+--
+-- Indices de la tabla `programacion`
+--
+ALTER TABLE `programacion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_semestre` (`id_semestre`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `programas`
+--
+ALTER TABLE `programas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `semestre`
+--
+ALTER TABLE `semestre`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `users`
@@ -133,16 +234,46 @@ ALTER TABLE `asignaturas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT de la tabla `materias`
+--
+ALTER TABLE `materias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `matricula`
+--
+ALTER TABLE `matricula`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `notas`
 --
 ALTER TABLE `notas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT de la tabla `programacion`
+--
+ALTER TABLE `programacion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `programas`
+--
+ALTER TABLE `programas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `semestre`
+--
+ALTER TABLE `semestre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Restricciones para tablas volcadas
@@ -155,11 +286,31 @@ ALTER TABLE `asignaturas`
   ADD CONSTRAINT `asignaturas_ibfk_1` FOREIGN KEY (`profesor_id`) REFERENCES `users` (`id`);
 
 --
+-- Filtros para la tabla `materias`
+--
+ALTER TABLE `materias`
+  ADD CONSTRAINT `materias_ibfk_1` FOREIGN KEY (`id_programa`) REFERENCES `programas` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `matricula`
+--
+ALTER TABLE `matricula`
+  ADD CONSTRAINT `matricula_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `matricula_ibfk_2` FOREIGN KEY (`id_semestre`) REFERENCES `semestre` (`id`) ON DELETE CASCADE;
+
+--
 -- Filtros para la tabla `notas`
 --
 ALTER TABLE `notas`
   ADD CONSTRAINT `notas_ibfk_1` FOREIGN KEY (`estudiante_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `notas_ibfk_2` FOREIGN KEY (`asignatura_id`) REFERENCES `asignaturas` (`id`);
+
+--
+-- Filtros para la tabla `programacion`
+--
+ALTER TABLE `programacion`
+  ADD CONSTRAINT `programacion_ibfk_1` FOREIGN KEY (`id_semestre`) REFERENCES `semestre` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `programacion_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
